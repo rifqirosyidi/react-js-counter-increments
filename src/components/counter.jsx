8e9a35d0efa99e94034a7d768431bc1c42918c64
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-08 18:54:07 +0700

    mapping

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,28 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count:0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
     render() {
         return (
             <React.Fragment>
-                <span className="badge badge-primary m-2">{ this.formatCount() }</span>
+                <span className={this.getBadgeClasses()}> { this.formatCount() }</span>
                 <button className="btn btn-secondary btn-sm">Increments</button>
+                <ul>
+                    { this.state.tags.map(tag => 
+                    <li key={tag}>{ tag }</li> 
+                    )}
+                </ul>
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
