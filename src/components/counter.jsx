f8ed518ceb673d61b34a27a839e5ab92c6713823
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-08 18:03:46 +0700

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    state = {
+        count:0
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <span className="badge badge-primary m-2">{ this.formatCount() }</span>
+                <button className="btn btn-secondary btn-sm">Increments</button>
+            </React.Fragment>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
+
+export default Counter;
